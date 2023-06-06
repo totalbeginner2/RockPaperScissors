ea003d11ea74f215d94bfdd385f1981d41d5f851
depedpc <rs200m2@yahoo.com>
2023-06-06 14:07:03 +0800

    added functions that turns the game into 5 rounds, counts the score between the user and computer, then displays score at the end

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-
+let userScore = 0;
+let computerScore = 0;
+let playerSelection;
+let computerSelection;
 
 function getComputerChoice(){
     let num = Math.floor(Math.random()*3);
@@ -27,38 +30,50 @@ function playRound(playerSelection, computerSelection){
     else if(playerSelection === "rock"){
             if(computerSelection === "scissor"){
                 result = "You win! Rock beats Scissor!"
+                userScore+=1;
             }
             else if(computerSelection === "paper"){
                 result = "You lose!, Paper beats Rock!"
+                computerScore+=1;
             }
         }
     else if(playerSelection === "paper"){
             if(computerSelection === "scissor"){
                 result = "You lose! Scissor beats paper!"
+                computerScore+=1;
             }
             else if(computerSelection === "rock"){
                 result = "You win!, Paper beats Rock!"
+                userScore+=1;
             }
         }
     else if(playerSelection === "scissor"){
             if(computerSelection === "rock"){
                 result = "You lose! Rock beats scissor"
+                computerScore+=1;
             }
             else if(computerSelection === "paper"){
-                result = "You win! Paper beats Rock!"
+                result = "You win! Scissor beats rocks!"
+                userScore += 1;
             }
         }
     return result;
     
 }
 
+function game(playRound){
+    for(let x=0; x<5; x++){
+        playerSelection = prompt("Rock, Paper, or Scissors? ", "Rock").toLowerCase();
+        computerSelection = getComputerChoice();
+        console.log("You picked " + playerSelection);
+        console.log("Computer picks " + computerSelection);
+        console.log(playRound(playerSelection,computerSelection));
+    }
 
-    
+    console.log("User score: "+ userScore +" Computer score: "+ computerScore);
+
+}
 
 
-const playerSelection = prompt("Rock, Paper, or Scissors? ", "Rock").toLowerCase();
-const computerSelection = getComputerChoice();
+game(playRound);
 
-console.log("You picked " + playerSelection);
-console.log("Computer picks " + computerSelection);
-console.log(playRound(playerSelection,computerSelection));
